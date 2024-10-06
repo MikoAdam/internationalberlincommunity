e8ff516b9a94babc1bb0e1893883fdc000709fe3
Adam Miko <adammiko98@gmail.com>
2024-10-06 22:22:46 +0200

    code fixes, telegram added

--- components/SocialMediaButtons.js
+++ components/SocialMediaButtons.js
@@ -56,6 +56,21 @@ const SocialMediaButtons = () => {
           <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M20 18h-2V9.25L12 13L6 9.25V18H4V6h1.2l6.8 4.25L18.8 6H20m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2"/></svg>
         </span>
       </button>
+      {/* Telegram Button */}
+      <button
+        type="button"
+        data-twe-ripple-init
+        data-twe-ripple-color="light"
+        className="flex items-center justify-center rounded bg-[#0088cc] px-2.5 py-2.5 text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+        onClick={() => window.open("https://t.me/internationalberlin", "_blank")}
+      >
+        <span className="[&>svg]:h-4 [&>svg]:w-4">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-telegram" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
+          </svg>
+        </span>
+      </button>
+
     </div>
   );
 };
