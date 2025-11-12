d858fbb6ca3002652be0b948bf05199bb1242f3d
Adam Miko <adammiko98@gmail.com>
2025-11-12 22:31:51 +0100

    auth removed

--- components/LayoutClient.js
+++ components/LayoutClient.js
@@ -3,7 +3,6 @@
 import { Toaster } from "react-hot-toast";
 import { Tooltip } from "react-tooltip";
 import NextTopLoader from "nextjs-toploader";
-import { SessionProvider } from "next-auth/react";
 import config from "@/config";
 import { LanguageProvider } from "./LanguageContext";
 
@@ -11,20 +10,18 @@ const ClientLayout = ({ children }) => {
   return (
     <>
       <NextTopLoader color={config.colors.main} showSpinner={false} />
-      <SessionProvider>
-        <LanguageProvider>
-          {children}
-          <Toaster
-            toastOptions={{
-              duration: 3000,
-            }}
-          />
-          <Tooltip 
-            id="tooltip" 
-            className="z-[60] !opacity-100 max-w-sm break-words"
-          />
-        </LanguageProvider>
-      </SessionProvider>
+      <LanguageProvider>
+        {children}
+        <Toaster
+          toastOptions={{
+            duration: 3000,
+          }}
+        />
+        <Tooltip
+          id="tooltip"
+          className="z-[60] !opacity-100 max-w-sm break-words"
+        />
+      </LanguageProvider>
     </>
   );
 };
